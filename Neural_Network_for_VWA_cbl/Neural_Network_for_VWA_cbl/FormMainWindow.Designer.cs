ce6fc0404e99282a6c8eadc748d76aba8b72535e
ChristianLeisch <101044837+ChristianLeisch@users.noreply.github.com>
2022-03-06 15:37:39 +0100

    Add files via upload

---
+++ Neural_Network_for_VWA_cbl/Neural_Network_for_VWA_cbl/FormMainWindow.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace NeuralNetwork
+{
+    partial class Form_main_window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInit = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btnTrain = new System.Windows.Forms.Button();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.lbltest = new System.Windows.Forms.Label();
+            this.lblacc = new System.Windows.Forms.Label();
+            this.btnimage = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.lblout = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnInit
+            // 
+            this.btnInit.Location = new System.Drawing.Point(46, 112);
+            this.btnInit.Name = "btnInit";
+            this.btnInit.Size = new System.Drawing.Size(120, 32);
+            this.btnInit.TabIndex = 0;
+            this.btnInit.Text = "Init";
+            this.btnInit.UseVisualStyleBackColor = true;
+            this.btnInit.Click += new System.EventHandler(this.btnInit_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(484, 120);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(0, 17);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Click += new System.EventHandler(this.lbl1_Click);
+            // 
+            // btnTrain
+            // 
+            this.btnTrain.Location = new System.Drawing.Point(46, 150);
+            this.btnTrain.Name = "btnTrain";
+            this.btnTrain.Size = new System.Drawing.Size(120, 32);
+            this.btnTrain.TabIndex = 2;
+            this.btnTrain.Text = "Train";
+            this.btnTrain.UseVisualStyleBackColor = true;
+            this.btnTrain.Click += new System.EventHandler(this.btnTrain_Click);
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(46, 188);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(120, 32);
+            this.btnTest.TabIndex = 3;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(172, 155);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(278, 155);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(384, 155);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(172, 193);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(278, 193);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 8;
+            // 
+            // lbltest
+            // 
+            this.lbltest.AutoSize = true;
+            this.lbltest.Location = new System.Drawing.Point(490, 163);
+            this.lbltest.Name = "lbltest";
+            this.lbltest.Size = new System.Drawing.Size(0, 17);
+            this.lbltest.TabIndex = 9;
+            // 
+            // lblacc
+            // 
+            this.lblacc.AutoSize = true;
+            this.lblacc.Location = new System.Drawing.Point(384, 196);
+            this.lblacc.Name = "lblacc";
+            this.lblacc.Size = new System.Drawing.Size(0, 17);
+            this.lblacc.TabIndex = 10;
+            // 
+            // btnimage
+            // 
+            this.btnimage.Location = new System.Drawing.Point(46, 226);
+            this.btnimage.Name = "btnimage";
+            this.btnimage.Size = new System.Drawing.Size(120, 32);
+            this.btnimage.TabIndex = 11;
+            this.btnimage.Text = "Image";
+            this.btnimage.UseVisualStyleBackColor = true;
+            this.btnimage.Click += new System.EventHandler(this.btnimage_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(642, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(599, 407);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(172, 231);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 13;
+            // 
+            // lblout
+            // 
+            this.lblout.AutoSize = true;
+            this.lblout.Location = new System.Drawing.Point(46, 261);
+            this.lblout.Name = "lblout";
+            this.lblout.Size = new System.Drawing.Size(0, 17);
+            this.lblout.TabIndex = 14;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(172, 117);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(283, 22);
+            this.textBox7.TabIndex = 15;
+            this.textBox7.Text = "E:\\VWA\\datasets\\mnist_train.csv";
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // Form_main_window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1335, 503);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.lblout);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnimage);
+            this.Controls.Add(this.lblacc);
+            this.Controls.Add(this.lbltest);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.btnTrain);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnInit);
+            this.Name = "Form_main_window";
+            this.Text = "Neural Network by Christian Leisch";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInit;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button btnTrain;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label lbltest;
+        private System.Windows.Forms.Label lblacc;
+        private System.Windows.Forms.Button btnimage;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label lblout;
+        private System.Windows.Forms.TextBox textBox7;
+    }
+}
+
---
+++ Neural_Network_for_VWA_cbl/Neural_Network_for_VWA_cbl/FormMainWindow.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NeuralNetwork
+{
+    public partial class Form_main_window : Form
+    {
+        public Form_main_window()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        const int numberpics = 60000;
+        
+
+        const int inputnodes = 784;
+        const int hiddennodes = 100;
+        const int outputnodes = 10;
+        const double learningrate = 0.2;
+
+        double[,] wih = new double[hiddennodes, inputnodes];
+        double[,] who = new double[outputnodes, hiddennodes];
+
+        double[] hiddenin = new double[hiddennodes];
+        double[] hiddenout = new double[hiddennodes];
+
+        double[] lastin = new double[outputnodes];
+        double[] lastout = new double[outputnodes];
+
+        double[,] images = new double[numberpics, 785];
+
+        Random random = new Random();
+
+        private void btnInit_Click(object sender, EventArgs e)
+        {
+            string Path = @"E:\VWA\datasets\mnist_test.csv";
+            string Path1 = @""+textBox7.Text;
+            var reader = new StreamReader(File.OpenRead(Path1));
+            for(int i =0; i<numberpics;i++)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(',');
+                int[] vals = Array.ConvertAll(values, int.Parse);
+                images[i, 0] = vals[0];
+                for (int j = 1; j < 785; j++)
+                {
+                    images[i, j] = (Convert.ToDouble(vals[j])/255*0.99)+0.01;
+                }
+            }
+            
+            for(int i =0;i<inputnodes; i++)
+            {
+                for (int j = 0; j < hiddennodes; j++)
+                {
+                    wih[j, i] = (Convert.ToDouble(random.Next(0, 20)) - 10) / 10;
+                }
+            }
+
+            for(int i = 0; i < hiddennodes; i++)
+            {
+                for (int j = 0; j < outputnodes; j++)
+                {
+                    who[j, i] = (Convert.ToDouble(random.Next(0, 20)) - 10) / 10;
+                }
+            }
+            lbl1.Text = "init done";
+        }
+
+        private void calculate(int n)
+        {
+            for(int i =0; i<hiddennodes;i++)
+            {
+                hiddenin[i] = 0;
+                for (int j = 0; j < inputnodes; j++)
+                {
+                    hiddenin[i] += images[n, j + 1] * wih[i,j];
+                }
+                hiddenout[i] = sigmoid(hiddenin[i]);
+            }
+
+            for (int i = 0; i < outputnodes; i++)
+            {
+                lastin[i] = 0;
+                for (int j = 0; j < hiddennodes; j++)
+                {
+                    lastin[i] += hiddenout[j] * who[i, j];
+                }
+                lastout[i] = sigmoid(lastin[i]);
+             }
+        }
+
+        private double sigmoid(double sum)
+        {
+            return 1 / (1 + Math.Pow(Math.E, (-sum)));
+        }
+
+        private void train(int n)
+        {
+            calculate(n);
+            double[] oerrors = new double[outputnodes];
+            double[] herrors = new double[hiddennodes];
+
+            for(int i =0;i<outputnodes;i++)
+            {
+                if((i)==images[n,0])
+                {
+                    oerrors[i] = 0.99 - lastout[i];
+                }
+                else
+                {
+                    oerrors[i] = 0.01 - lastout[i];
+                }
+            }
+
+            for(int i=0;i<hiddennodes;i++)
+            {
+                for(int j =0;j<outputnodes;j++)
+                {
+                    herrors[i] += oerrors[j] * who[j, i];
+                }
+            }
+
+            for(int j =0;j<outputnodes;j++)
+            {
+                for(int i =0;i<hiddennodes;i++)
+                {
+                    who[j, i] += learningrate * oerrors[j] * lastout[j] * (1 - lastout[j]) * hiddenout[i];
+                }
+            }
+            
+            for (int j = 0; j < hiddennodes; j++)
+            {
+                for (int i = 0; i < inputnodes; i++)
+                {
+                    wih[j, i] += learningrate * herrors[j] * hiddenout[j] * (1 - hiddenout[j]) * images[n,i+1]; //nochmal checken, Seite 86
+                }
+            }
+            
+            
+        }
+
+        private void btnTrain_Click(object sender, EventArgs e)
+        {
+            int o = Int32.Parse(textBox1.Text);
+            int n = Int32.Parse(textBox2.Text);
+            int p = Int32.Parse(textBox3.Text);
+            int c = (n - o) * p;
+            int r = 0;
+            for (int j = 0; j < p; j++)
+            {
+                for (int i = o; i < n; i++)
+                {
+
+                    train(i);
+                    r++;
+                    showPercentage(r, c);
+                }
+            }
+        }
+
+        private void showPercentage(int i, int a)
+        {
+            lbltest.Text = i + "/" + a;
+            lbltest.Invalidate();
+            lbltest.Update();
+            lbltest.Refresh();
+            Application.DoEvents();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            int o = Int32.Parse(textBox4.Text);
+            int n = Int32.Parse(textBox5.Text);
+            double correct = 0;
+            for (int i = o; i < n; i++)
+            {
+                correct = correct + test(i);
+            }
+            lblacc.Text = "accuracy:" + correct / (n - o) * 100 +"%";
+        }
+
+        private int test(int n)
+        {
+            int realnumber = Convert.ToInt32(images[n, 0]);
+            calculate(n);
+            int max = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (lastout[i] > lastout[max])
+                {
+                    max = i;
+                }
+            }
+            if (max == realnumber)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+        private int predict(int n)
+        {
+
+            calculate(n);
+            int max = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (lastout[i] > lastout[max])
+                {
+                    max = i;
+                }
+            }
+            return max;
+        }
+
+        private void btnimage_Click(object sender, EventArgs e)
+        {
+            int m = Int32.Parse(textBox6.Text);
+            int scale = 8;
+            Bitmap bitimage = new Bitmap(28, 28);
+            for (int Xcount = 0; Xcount < 28; Xcount++)
+            {
+                for (int Ycount = 0; Ycount < 28; Ycount++)
+                {
+                    int address = Ycount * 28 + Xcount + 1;
+                    int color = Convert.ToInt32((images[m, address]-0.01)*255/0.99);
+                    bitimage.SetPixel(Xcount, Ycount, Color.FromArgb(color, color, color));
+                }
+            }
+
+            Bitmap scaledbitimage = new Bitmap(28 * scale, 28 * scale);
+            for (int Xcount = 0; Xcount < 28; Xcount++)
+            {
+                for (int Ycount = 0; Ycount < 28; Ycount++)
+                {
+                    Color color = bitimage.GetPixel(Xcount, Ycount);
+                    for (int k = 0; k < 8; k++)
+                    {
+                        for (int l = 0; l < 8; l++)
+                        {
+                            int x = Xcount * 8 + k;
+                            int y = Ycount * 8 + l;
+                            scaledbitimage.SetPixel(x, y, color);
+                        }
+                    }
+                }
+
+            }
+            pictureBox1.Image = scaledbitimage;
+            lblout.Text = null;
+            calculate(m);
+            for (int i = 0; i < 10; i++)
+            {
+                lblout.Text = lblout.Text + i + ":" + lastout[i] + "\n";
+            }
+            lblout.Text = lblout.Text + "predicted: " + predict(m) + ", real:" + images[m, 0];
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
